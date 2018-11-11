870b178cf07ef9b750406d9c354dea71d399a3ce
Pierre Rognon <pierre@ulica.fr>
2018-11-11 19:51:40 +0100

    [CineLog] add ask of storage permission in export activity + fix not internationalized strings

--- app/src/main/java/com/ulicae/cinelog/exportdb/ExportDb.java
+++ app/src/main/java/com/ulicae/cinelog/exportdb/ExportDb.java
@@ -1,13 +1,19 @@
 package com.ulicae.cinelog.exportdb;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
 
 import com.ulicae.cinelog.KinoApplication;
+import com.ulicae.cinelog.MainActivity;
 import com.ulicae.cinelog.R;
 import com.ulicae.cinelog.db.LocalKinoRepository;
 
@@ -44,9 +50,14 @@ public class ExportDb extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
+    private Boolean writeStoragePermission;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ActivityCompat.requestPermissions(ExportDb.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
         setContentView(R.layout.activity_export_db);
         ButterKnife.bind(this);
 
@@ -55,36 +66,53 @@ public class ExportDb extends AppCompatActivity {
 
     @OnClick(R.id.export_db_button)
     public void onClick(View view) {
-        Toast.makeText(getApplicationContext(), "Export launched !", Toast.LENGTH_LONG).show();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
-        CsvExporter csvExporter;
-        FileWriter fileWriter;
-        try {
-            fileWriter = new FileWriterGetter().get("export.csv");
-        } catch (IOException e) {
-            Toast.makeText(getApplicationContext(), "An error occured while trying to get export file !", Toast.LENGTH_LONG).show();
-            return;
+        if(writeStoragePermission != null && writeStoragePermission) {
+            Toast.makeText(getApplicationContext(), prefs.getString("export_start_toast", null), Toast.LENGTH_LONG).show();
+
+            CsvExporter csvExporter;
+            FileWriter fileWriter;
+            try {
+                fileWriter = new FileWriterGetter().get("export.csv");
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), prefs.getString("export_io_error_toast", null), Toast.LENGTH_LONG).show();
+                return;
+            }
+            try {
+                csvExporter = new CsvExporter(
+                        new LocalKinoRepository(((KinoApplication) getApplication()).getDaoSession()),
+                        fileWriter
+                );
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), prefs.getString("export_io_error_toast", null), Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            try {
+                csvExporter.export();
+
+                Toast.makeText(getApplicationContext(), prefs.getString("export_succeeded_toast", null), Toast.LENGTH_LONG).show();
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), prefs.getString("export_io_error_toast", null), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), prefs.getString("export_permission_error_toast", null), Toast.LENGTH_LONG).show();
         }
-        try {
-            csvExporter = new CsvExporter(
-                    new LocalKinoRepository(((KinoApplication) getApplication()).getDaoSession()),
-                    fileWriter
-            );
-        } catch (IOException e) {
-            Toast.makeText(getApplicationContext(), "An error occured while creating export file !", Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        try {
-            csvExporter.export();
-
-            Toast.makeText(getApplicationContext(), "Export succeeded !", Toast.LENGTH_LONG).show();
-        } catch (IOException e) {
-            Toast.makeText(getApplicationContext(), "An error occured while exporting !", Toast.LENGTH_LONG).show();
-        }
-
-
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @SuppressWarnings("NullableProblems") String permissions[],
+                                           @SuppressWarnings("NullableProblems") int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                writeStoragePermission = grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+            }
+        }
+    }
+
+
 
 
     @Override
