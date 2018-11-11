b81fcd5d4a00613f23b6a24a29b9cad238829ced
Pierre Rognon <pierre@ulica.fr>
2018-11-11 19:28:03 +0100

    [CineLog] change how notes are displayed in kino view

--- app/src/main/java/com/ulicae/cinelog/ViewKino.java
+++ app/src/main/java/com/ulicae/cinelog/ViewKino.java
@@ -29,22 +29,21 @@ import butterknife.OnClick;
 /**
  * CineLog Copyright 2018 Pierre Rognon
  * kinolog Copyright (C) 2017  ryan rigby
- *
- *
+ * <p>
+ * <p>
  * This file is part of CineLog.
  * CineLog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * CineLog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with CineLog. If not, see <https://www.gnu.org/licenses/>.
- *
  */
 public class ViewKino extends AppCompatActivity {
     @BindView(R.id.toolbar)
@@ -62,6 +61,8 @@ public class ViewKino extends AppCompatActivity {
     TextView overview;
     @BindView(R.id.view_kino_review_rating)
     RatingBar rating;
+    @BindView(R.id.view_kino_review_rating_as_text)
+    TextView ratingAsText;
     @BindView(R.id.view_kino_review_review)
     TextView review;
     @BindView(R.id.view_kino_review_review_date)
@@ -105,6 +106,8 @@ public class ViewKino extends AppCompatActivity {
             rating.setRating(kino.getRating());
         }
         rating.setStepSize(0.5f);
+
+        ratingAsText.setText(String.format("%s", kino.getRating()));
     }
 
     @Override
