137459b3998098ceb412cb61e20e2d359be6c583
Pierre Rognon <pierre@ulica.fr>
2018-11-28 13:37:52 +0100

    [Cinelog] add missing license

--- app/src/main/java/com/ulicae/cinelog/android/activities/add/AddSerieActivity.java
+++ app/src/main/java/com/ulicae/cinelog/android/activities/add/AddSerieActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 
 import com.ulicae.cinelog.KinoApplication;
 import com.ulicae.cinelog.R;
