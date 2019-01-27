e89bc7e1b6958b0d6b484e08bdc8fbae57175fe2
Pierre Rognon <pierre@ulica.fr>
2019-01-27 15:33:36 +0100

    [CineLog / Wishlist] add missing licenses

--- app/src/main/java/com/ulicae/cinelog/data/dto/data/MovieToWishlistDataDtoBuilder.java
+++ app/src/main/java/com/ulicae/cinelog/data/dto/data/MovieToWishlistDataDtoBuilder.java
@@ -9,6 +9,25 @@ import com.uwetrottmann.tmdb2.entities.Movie;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * CineLog Copyright 2019 Pierre Rognon
+ *
+ *
+ * This file is part of CineLog.
+ * CineLog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CineLog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CineLog. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 public class MovieToWishlistDataDtoBuilder {
 
     public WishlistDataDto build(BaseMovie movie) {
--- app/src/main/java/com/ulicae/cinelog/data/dto/data/WishlistSerieToSerieDataDtoBuilder.java
+++ app/src/main/java/com/ulicae/cinelog/data/dto/data/WishlistSerieToSerieDataDtoBuilder.java
@@ -3,6 +3,25 @@ package com.ulicae.cinelog.data.dto.data;
 import com.ulicae.cinelog.data.dao.TmdbSerie;
 import com.ulicae.cinelog.data.dao.WishlistSerie;
 
+/**
+ * CineLog Copyright 2019 Pierre Rognon
+ *
+ *
+ * This file is part of CineLog.
+ * CineLog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CineLog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CineLog. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 public class WishlistSerieToSerieDataDtoBuilder {
 
     public WishlistDataDto build(WishlistSerie wishlistSerie) {
