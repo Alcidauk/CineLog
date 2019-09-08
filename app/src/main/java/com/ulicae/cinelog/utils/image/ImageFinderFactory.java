1bb47b7460422bfeba5db6dd56745eadd9bc2f76
Pierre Rognon <pierre@ulica.fr>
2019-09-08 18:16:50 +0200

    [ CineLog / Network ] clean class visibility

--- app/src/main/java/com/ulicae/cinelog/utils/image/ImageFinderFactory.java
+++ app/src/main/java/com/ulicae/cinelog/utils/image/ImageFinderFactory.java
@@ -1,6 +1,6 @@
 package com.ulicae.cinelog.utils.image;
 
-public class ImageFinderFactory {
+class ImageFinderFactory {
 
     ImageFinder makeNetworkImageFinder(){
         return new ImageNetworkFinder();
