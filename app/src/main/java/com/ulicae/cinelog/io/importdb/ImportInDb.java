5c763dc61c918f67fb24244ea80c3af09b086c65
Pierre Rognon <pierre.service@ulicae.com>
2022-09-15 11:22:04 +0200

    [ CineLog / View Binding ] migrate ImportInDbActivity from butterknife to ViewBinding

--- app/src/main/java/com/ulicae/cinelog/io/importdb/ImportInDb.java
+++ app/src/main/java/com/ulicae/cinelog/io/importdb/ImportInDb.java
@@ -3,12 +3,11 @@ package com.ulicae.cinelog.io.importdb;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import androidx.core.app.ActivityCompat;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.ulicae.cinelog.KinoApplication;
 import com.ulicae.cinelog.R;
@@ -17,18 +16,15 @@ import com.ulicae.cinelog.data.services.reviews.SerieService;
 import com.ulicae.cinelog.data.services.tags.TagService;
 import com.ulicae.cinelog.data.services.wishlist.MovieWishlistService;
 import com.ulicae.cinelog.data.services.wishlist.SerieWishlistService;
+import com.ulicae.cinelog.databinding.ActivityImportDbBinding;
 import com.ulicae.cinelog.io.importdb.builder.KinoDtoFromRecordBuilder;
 import com.ulicae.cinelog.io.importdb.builder.SerieDtoFromRecordBuilder;
 import com.ulicae.cinelog.io.importdb.builder.TagDtoFromRecordBuilder;
 import com.ulicae.cinelog.io.importdb.builder.WishlistDtoFromRecordBuilder;
 import com.ulicae.cinelog.utils.ThemeWrapper;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-
 /**
- * CineLog Copyright 2018 Pierre Rognon
+ * CineLog Copyright 2022 Pierre Rognon
  * <p>
  * <p>
  * This file is part of CineLog.
@@ -47,30 +43,7 @@ import butterknife.OnClick;
  */
 public class ImportInDb extends AppCompatActivity {
 
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-
-    @BindView(R.id.import_tags_status_waiting)
-    TextView tag_status;
-    @BindView(R.id.import_movies_status_waiting)
-    TextView movie_status;
-    @BindView(R.id.import_series_status_waiting)
-    TextView serie_status;
-    @BindView(R.id.import_wishlist_movies_status_waiting)
-    TextView wishlist_movie_status;
-    @BindView(R.id.import_wishlist_series_status_waiting)
-    TextView wishlist_serie_status;
-
-    @BindView(R.id.import_tags_error_message)
-    TextView tag_error;
-    @BindView(R.id.import_movies_error_message)
-    TextView movie_error;
-    @BindView(R.id.import_series_error_message)
-    TextView serie_error;
-    @BindView(R.id.import_wishlist_movies_error_message)
-    TextView wishlist_movie_error;
-    @BindView(R.id.import_wishlist_series_error_message)
-    TextView wishlist_serie_error;
+    private ActivityImportDbBinding binding;
 
     private Boolean writeStoragePermission;
 
@@ -81,17 +54,19 @@ public class ImportInDb extends AppCompatActivity {
 
         ActivityCompat.requestPermissions(ImportInDb.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
 
-        setContentView(R.layout.activity_import_db);
-        ButterKnife.bind(this);
+        binding = ActivityImportDbBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        setSupportActionBar(toolbar);
+        setSupportActionBar(binding.importInDbToolbar.toolbar);
+
+        binding.importInDbContent.importDbButton.setOnClickListener(this::onClick);
     }
 
-    @OnClick(R.id.import_db_button)
     public void onClick(View view) {
         if (writeStoragePermission != null && writeStoragePermission) {
             Toast.makeText(getApplicationContext(), getString(R.string.import_starting_toast), Toast.LENGTH_SHORT).show();
 
+            // TODO improve this code
             try {
                 new CsvImporter<>(
                         new FileReaderGetter(getApplication()),
@@ -99,13 +74,12 @@ public class ImportInDb extends AppCompatActivity {
                         new TagService(((KinoApplication) getApplication()).getDaoSession()),
                         this).importCsvFile("import_tags.csv");
 
-                tag_status.setText(R.string.import_status_success);
+                binding.importInDbContent.importTagsStatusWaiting.setText(R.string.import_status_success);
             } catch (ImportException e) {
                 Toast.makeText(getApplication().getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                tag_status.setText(R.string.import_status_failed);
-                tag_error.setText(e.getMessage());
+                binding.importInDbContent.importTagsStatusWaiting.setText(R.string.import_status_failed);
+                binding.importInDbContent.importTagsErrorMessage.setText(e.getMessage());
             }
-
 
             try {
                 new CsvImporter<>(
@@ -114,11 +88,11 @@ public class ImportInDb extends AppCompatActivity {
                         new KinoService(((KinoApplication) getApplication()).getDaoSession()),
                         this).importCsvFile("import_movies.csv");
 
-                movie_status.setText(R.string.import_status_success);
+                binding.importInDbContent.importMoviesStatusWaiting.setText(R.string.import_status_success);
             } catch (ImportException e) {
                 Toast.makeText(getApplication().getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                movie_status.setText(R.string.import_status_failed);
-                movie_error.setText(e.getMessage());
+                binding.importInDbContent.importMoviesStatusWaiting.setText(R.string.import_status_failed);
+                binding.importInDbContent.importMoviesErrorMessage.setText(e.getMessage());
             }
 
             try {
@@ -128,12 +102,11 @@ public class ImportInDb extends AppCompatActivity {
                         new SerieService(((KinoApplication) getApplication()).getDaoSession(), this),
                         this).importCsvFile("import_series.csv");
 
-                serie_status.setText(R.string.import_status_success);
+                binding.importInDbContent.importSeriesStatusWaiting.setText(R.string.import_status_success);
             } catch (ImportException e) {
                 Toast.makeText(getApplication().getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                serie_status.setText(R.string.import_status_failed);
-                serie_error.setText(e.getMessage());
-
+                binding.importInDbContent.importSeriesStatusWaiting.setText(R.string.import_status_failed);
+                binding.importInDbContent.importSeriesErrorMessage.setText(e.getMessage());
             }
 
             try {
@@ -143,12 +116,11 @@ public class ImportInDb extends AppCompatActivity {
                         new MovieWishlistService(((KinoApplication) getApplication()).getDaoSession()),
                         this).importCsvFile("import_wishlist_movies.csv");
 
-                wishlist_serie_status.setText(R.string.import_status_success);
+                binding.importInDbContent.importWishlistMoviesStatusWaiting.setText(R.string.import_status_success);
             } catch (ImportException e) {
                 Toast.makeText(getApplication().getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                wishlist_serie_status.setText(R.string.import_status_failed);
-                wishlist_serie_error.setText(e.getMessage());
-
+                binding.importInDbContent.importWishlistMoviesStatusWaiting.setText(R.string.import_status_failed);
+                binding.importInDbContent.importWishlistMoviesErrorMessage.setText(e.getMessage());
             }
 
             try {
@@ -158,11 +130,11 @@ public class ImportInDb extends AppCompatActivity {
                         new SerieWishlistService(((KinoApplication) getApplication()).getDaoSession()),
                         this).importCsvFile("import_wishlist_series.csv");
 
-                wishlist_movie_status.setText(R.string.import_status_success);
+                binding.importInDbContent.importWishlistSeriesStatusWaiting.setText(R.string.import_status_success);
             } catch (ImportException e) {
                 Toast.makeText(getApplication().getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                wishlist_movie_status.setText(R.string.import_status_failed);
-                wishlist_movie_error.setText(e.getMessage());
+                binding.importInDbContent.importWishlistSeriesStatusWaiting.setText(R.string.import_status_failed);
+                binding.importInDbContent.importWishlistSeriesErrorMessage.setText(e.getMessage());
             }
 
             Toast.makeText(getApplicationContext(), getString(R.string.import_ending_toast), Toast.LENGTH_SHORT).show();
@@ -177,10 +149,8 @@ public class ImportInDb extends AppCompatActivity {
                                            @SuppressWarnings("NullableProblems") String permissions[],
                                            @SuppressWarnings("NullableProblems") int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case 1: {
-                writeStoragePermission = grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
-            }
+        if (requestCode == 1) {
+            writeStoragePermission = grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
         }
     }
 
