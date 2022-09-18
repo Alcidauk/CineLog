7bf86d98634a75d299b0b920392077b0a665af18
Pierre Rognon <pierre.service@ulicae.com>
2022-09-18 18:48:39 +0200

    [ CineLog / View Binding ] migrate ViewUnregisteredKino from butterknife to ViewBinding

--- app/src/main/java/com/ulicae/cinelog/android/activities/ViewUnregisteredKino.java
+++ app/src/main/java/com/ulicae/cinelog/android/activities/ViewUnregisteredKino.java
@@ -3,113 +3,96 @@ package com.ulicae.cinelog.android.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.ulicae.cinelog.R;
 import com.ulicae.cinelog.data.dto.KinoDto;
+import com.ulicae.cinelog.databinding.ActivityViewUnregisteredKinoBinding;
+import com.ulicae.cinelog.databinding.ContentKinoViewUnregisteredBinding;
 import com.ulicae.cinelog.utils.ThemeWrapper;
 
 import org.parceler.Parcels;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-
 /**
- * CineLog Copyright 2018 Pierre Rognon
+ * CineLog Copyright 2022 Pierre Rognon
  * kinolog Copyright (C) 2017  ryan rigby
- *
- *
+ * <p>
+ * <p>
  * This file is part of CineLog.
  * CineLog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * CineLog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with CineLog. If not, see <https://www.gnu.org/licenses/>.
- *
  */
 public class ViewUnregisteredKino extends AppCompatActivity {
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-    @BindView(R.id.fab)
-    FloatingActionButton fab;
-
-    @BindView(R.id.view_kino_tmdb_image_layout)
-    ImageView poster;
-    @BindView(R.id.view_kino_tmdb_title)
-    TextView title;
-    @BindView(R.id.view_kino_tmdb_year)
-    TextView year;
-    @BindView(R.id.view_kino_tmdb_overview)
-    TextView overview;
 
     KinoDto kino;
     int position;
     boolean editted = false;
 
     private static final int RESULT_ADD_REVIEW = 3;
+    private ActivityViewUnregisteredKinoBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         new ThemeWrapper().setThemeWithPreferences(this);
 
-        setContentView(R.layout.activity_view_unregistered_kino);
-        ButterKnife.bind(this);
+
+        binding = ActivityViewUnregisteredKinoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.fab.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), EditReview.class);
+            intent.putExtra("kino", Parcels.wrap(kino));
+            intent.putExtra("dtoType", getIntent().getStringExtra("dtoType"));
+
+            startActivityForResult(intent, RESULT_ADD_REVIEW);
+        });
 
         kino = Parcels.unwrap(getIntent().getParcelableExtra("kino"));
         position = getIntent().getIntExtra("kino_position", -1);
 
         configureLabels(getIntent().getStringExtra("dtoType"));
 
-        setSupportActionBar(toolbar);
+        setSupportActionBar(binding.viewUnregisteredToolbar.toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     private void configureLabels(String dtoType) {
-        if(dtoType.equals("serie")){
+        if (dtoType.equals("serie")) {
             setTitle(R.string.title_activity_view_unregistered_serie);
         }
-    }
-
-    @OnClick(R.id.fab)
-    public void onClick(View view) {
-        Intent intent = new Intent(this, EditReview.class);
-        intent.putExtra("kino", Parcels.wrap(kino));
-        intent.putExtra("dtoType", getIntent().getStringExtra("dtoType"));
-
-        startActivityForResult(intent, RESULT_ADD_REVIEW);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
+        ContentKinoViewUnregisteredBinding viewUnregisteredContent = binding.viewUnregisteredContent;
+
         if (kino.getPosterPath() != null && !"".equals(kino.getPosterPath())) {
             Glide.with(this)
                     .load("https://image.tmdb.org/t/p/w185" + kino.getPosterPath())
                     .centerCrop()
                     .crossFade()
-                    .into(poster);
+                    .into(viewUnregisteredContent.viewKinoTmdbImageLayout);
         }
-        year.setText(kino.getReleaseDate());
-        overview.setText(kino.getOverview());
+        viewUnregisteredContent.viewKinoTmdbYear.setText(kino.getReleaseDate());
+        viewUnregisteredContent.viewKinoTmdbOverview.setText(kino.getOverview());
 
-        title.setText(kino.getTitle());
+        viewUnregisteredContent.viewKinoTmdbTitle.setText(kino.getTitle());
     }
 
     @Override
@@ -129,20 +112,18 @@ public class ViewUnregisteredKino extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                if (editted) {
-                    Intent returnIntent = getIntent();
-                    returnIntent.putExtra("dtoType", getIntent().getStringExtra("dtoType"));
-                    returnIntent.putExtra("kino", Parcels.wrap(kino));
-                    returnIntent.putExtra("kino_position", position);
-                    setResult(Activity.RESULT_OK, returnIntent);
-                }
-                onBackPressed();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        if (item.getItemId() == android.R.id.home) {
+            if (editted) {
+                Intent returnIntent = getIntent();
+                returnIntent.putExtra("dtoType", getIntent().getStringExtra("dtoType"));
+                returnIntent.putExtra("kino", Parcels.wrap(kino));
+                returnIntent.putExtra("kino_position", position);
+                setResult(Activity.RESULT_OK, returnIntent);
+            }
+            onBackPressed();
+            return true;
         }
+        return super.onOptionsItemSelected(item);
     }
 
 }
