5dd76cc0e42cf3c6e8a88792c7aeb6ca016c6e84
Pierre Rognon <pierre.service@ulicae.com>
2022-10-13 11:52:01 +0200

    [ CineLog / Fragments ] move UI logic in fragments for ViewSerie

--- app/src/main/java/com/ulicae/cinelog/android/activities/ViewSerie.java
+++ app/src/main/java/com/ulicae/cinelog/android/activities/ViewSerie.java
@@ -1,29 +1,17 @@
 package com.ulicae.cinelog.android.activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 
 import com.ulicae.cinelog.R;
-import com.ulicae.cinelog.android.activities.fragments.serie.SerieViewEpisodesFragment;
-import com.ulicae.cinelog.android.activities.fragments.serie.SerieViewGeneralFragment;
+import com.ulicae.cinelog.android.activities.fragments.ViewSerieFragment;
 import com.ulicae.cinelog.data.dto.KinoDto;
-import com.ulicae.cinelog.data.dto.SerieDto;
-import com.ulicae.cinelog.databinding.ActivityViewSerieBinding;
+import com.ulicae.cinelog.databinding.V2ViewSerieHostBinding;
 import com.ulicae.cinelog.utils.ThemeWrapper;
 
 import org.parceler.Parcels;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 /**
  * CineLog Copyright 2022 Pierre Rognon
@@ -45,102 +33,28 @@ import java.util.Objects;
  */
 public class ViewSerie extends AppCompatActivity {
 
-    public static final int RESULT_ADD_REVIEW = 3;
-
-    private SerieViewGeneralFragment generalFragment;
-
-    KinoDto kino;
-    int position;
-    boolean editted = false;
-
-    private ActivityViewSerieBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         new ThemeWrapper().setThemeWithPreferences(this);
 
-        binding = ActivityViewSerieBinding.inflate(getLayoutInflater());
+        V2ViewSerieHostBinding binding = V2ViewSerieHostBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.fab.setOnClickListener(view -> {
-            Intent intent = new Intent(getApplicationContext(), EditReview.class);
-            intent.putExtra("dtoType", getIntent().getStringExtra("dtoType"));
-            intent.putExtra("kino", Parcels.wrap(kino));
-            startActivityForResult(intent, RESULT_ADD_REVIEW);
-        });
-
-        kino = Parcels.unwrap(getIntent().getParcelableExtra("kino"));
-        position = getIntent().getIntExtra("kino_position", -1);
-
-        setSupportActionBar(binding.viewSerieToolbar.toolbar);
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-
-        setViewPager(binding.viewSerieContent.serieViewPager);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            if (editted) {
-                Intent returnIntent = getIntent();
-                returnIntent.putExtra("dtoType", getIntent().getStringExtra("dtoType"));
-                returnIntent.putExtra("kino", Parcels.wrap(kino));
-                returnIntent.putExtra("kino_position", position);
-                setResult(Activity.RESULT_OK, returnIntent);
-            }
-            onBackPressed();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RESULT_ADD_REVIEW && resultCode == Activity.RESULT_OK) {
-            kino = Parcels.unwrap(data.getParcelableExtra("kino"));
-            generalFragment.updateKino((SerieDto) kino);
+        if (savedInstanceState == null) {
+            ViewSerieFragment fragment = new ViewSerieFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.view_serie_host, fragment)
+                    .commit();
         }
     }
 
-    private void setViewPager(ViewPager viewPager) {
-        ViewSerie.ViewPagerAdapter adapter = new ViewSerie.ViewPagerAdapter(getSupportFragmentManager());
-
-        generalFragment = new SerieViewGeneralFragment();
-        adapter.addFragment(generalFragment, getString(R.string.title_fragment_serie_general));
-        adapter.addFragment(new SerieViewEpisodesFragment(), getString(R.string.title_fragment_serie_episodes));
-
-        viewPager.setAdapter(adapter);
-        binding.viewSerieToolbar.tabs.setupWithViewPager(viewPager);
-    }
-
-    class ViewPagerAdapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
-
-        public ViewPagerAdapter(FragmentManager manager) {
-            super(manager);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
-        }
+    public void goToSerieEdition(KinoDto kino) {
+        Intent intent = new Intent(getApplicationContext(), EditReview.class);
+        intent.putExtra("dtoType", getIntent().getStringExtra("dtoType"));
+        intent.putExtra("kino", Parcels.wrap(kino));
+        startActivity(intent);
     }
 }
