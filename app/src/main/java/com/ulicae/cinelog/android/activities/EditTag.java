ecd6cc155691484c0aef76ecbad8a2a2c3d9c987
Pierre Rognon <pierre.service@ulicae.com>
2022-10-13 12:18:22 +0200

    [ CineLog / Fragments ] move UI logic in fragments for EditTag

--- app/src/main/java/com/ulicae/cinelog/android/activities/EditTag.java
+++ app/src/main/java/com/ulicae/cinelog/android/activities/EditTag.java
@@ -1,27 +1,15 @@
 package com.ulicae.cinelog.android.activities;
 
 import android.annotation.SuppressLint;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.skydoves.colorpickerview.ColorPickerDialog;
-import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
-import com.ulicae.cinelog.KinoApplication;
 import com.ulicae.cinelog.R;
-import com.ulicae.cinelog.data.dto.TagDto;
-import com.ulicae.cinelog.data.services.tags.TagService;
-import com.ulicae.cinelog.databinding.ActivityAddTagBinding;
-import com.ulicae.cinelog.databinding.ContentAddTagBinding;
+import com.ulicae.cinelog.android.v2.EditTagFragment;
+import com.ulicae.cinelog.android.v2.ViewKinoFragment;
+import com.ulicae.cinelog.databinding.V2EditTagHostBinding;
 import com.ulicae.cinelog.utils.ThemeWrapper;
-
-import org.parceler.Parcels;
-
-import java.util.Objects;
 
 /**
  * CineLog Copyright 2022 Pierre Rognon
@@ -43,12 +31,6 @@ import java.util.Objects;
  */
 public class EditTag extends AppCompatActivity {
 
-    private ContentAddTagBinding binding;
-
-    TagDto tag;
-
-    private TagService tagDtoService;
-
 
     @SuppressLint("ResourceType")
     @Override
@@ -56,89 +38,17 @@ public class EditTag extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         new ThemeWrapper().setThemeWithPreferences(this);
 
-        ActivityAddTagBinding activityBinding = ActivityAddTagBinding.inflate(getLayoutInflater());
-        binding = activityBinding.addTagContent;
-        setContentView(activityBinding.getRoot());
+        V2EditTagHostBinding binding = V2EditTagHostBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        tagDtoService = new TagService(((KinoApplication) getApplication()).getDaoSession());
-
-        tag = Parcels.unwrap(getIntent().getParcelableExtra("tag"));
-        if (tag == null) {
-            tag = new TagDto();
-            tag.setColor(getString(R.color.colorPrimary));
-        } else {
-            binding.tagName.setText(tag.getName());
-            binding.tagFilms.setChecked(tag.isForMovies());
-            binding.tagSeries.setChecked(tag.isForSeries());
-        }
-
-        activityBinding.fabSaveTag.setOnClickListener(view -> EditTag.this.onClick());
-        binding.tagFilms.setOnCheckedChangeListener((compoundButton, b) -> onFilmsCheckedChanged(b));
-        binding.tagSeries.setOnCheckedChangeListener((compoundButton, b) -> onSeriesCheckedChanged(b));
-        binding.tagColorUpdate.setOnClickListener(this::onTagColorUpdate);
-
-        fetchColor();
-
-        setSupportActionBar(activityBinding.addTagToolbar.toolbar);
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        if (savedInstanceState == null) {
+            EditTagFragment fragment = new EditTagFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.edit_tag_host, fragment)
+                    .commit();
         }
     }
 
-    public void onClick() {
-        tag.setName(binding.tagName.getText().toString());
-
-        if (tag.getName() == null || tag.getName().isEmpty()) {
-            Toast.makeText(
-                            getBaseContext(),
-                            getBaseContext().getString(R.string.edit_tag_name_not_filled),
-                            Toast.LENGTH_LONG)
-                    .show();
-            return;
-        }
-
-        tagDtoService.createOrUpdate(tag);
-
-        finish();
-    }
-
-    public void onFilmsCheckedChanged(boolean checked) {
-        tag.setForMovies(checked);
-    }
-
-    public void onSeriesCheckedChanged(boolean checked) {
-        tag.setForSeries(checked);
-    }
-
-    private void fetchColor() {
-        if (tag.getColor() != null) {
-            binding.tagColorCurrent.setBackgroundColor(Color.parseColor(tag.getColor()));
-        }
-    }
-
-    public void onTagColorUpdate(View view) {
-        new ColorPickerDialog.Builder(this)
-                .setTitle("ColorPicker Dialog")
-                .setPreferenceName("MyColorPickerDialog")
-                .setPositiveButton(getString(R.string.save),
-                        (ColorEnvelopeListener) (envelope, fromUser) -> {
-                            tag.setColor("#" + envelope.getHexCode().substring(2));
-                            fetchColor();
-                        })
-                .setNegativeButton(getString(R.string.cancel), (dialogInterface, i) -> dialogInterface.dismiss())
-                .attachAlphaSlideBar(false)
-                .attachBrightnessSlideBar(false)
-                .setBottomSpace(12)
-                .show();
-    }
 
 }
