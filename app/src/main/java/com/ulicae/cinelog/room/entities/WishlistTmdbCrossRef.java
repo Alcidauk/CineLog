72ce0c3dccc941a887152d187e41b78c2182f844
Pierre R. <pierre+elitauk@rgnn.fr>
2024-06-25 15:25:40 +0200

    [Cinelog / Room] add equals

--- app/src/main/java/com/ulicae/cinelog/room/entities/WishlistTmdbCrossRef.java
+++ app/src/main/java/com/ulicae/cinelog/room/entities/WishlistTmdbCrossRef.java
@@ -2,6 +2,8 @@ package com.ulicae.cinelog.room.entities;
 
 import androidx.room.Entity;
 
+import java.util.Objects;
+
 @Entity(primaryKeys = {"wishlistId", "tmdbId"})
 public class WishlistTmdbCrossRef {
 
@@ -11,5 +13,18 @@ public class WishlistTmdbCrossRef {
     public WishlistTmdbCrossRef(long wishlistId, long tmdbId) {
         this.wishlistId = wishlistId;
         this.tmdbId = tmdbId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WishlistTmdbCrossRef that = (WishlistTmdbCrossRef) o;
+        return wishlistId == that.wishlistId && tmdbId == that.tmdbId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(wishlistId, tmdbId);
     }
 }
