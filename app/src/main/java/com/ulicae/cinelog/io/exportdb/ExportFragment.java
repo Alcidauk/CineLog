c33123f138ee1d1f908e05a0504e1ba117e8bbb9
Pierre R. <pierre+elitauk@rgnn.fr>
2024-08-28 14:59:27 +0200

    [Cinelog / Room] rewrite IO export mecanism -> remove permissions and only allow a dir to export files in it

--- app/src/main/java/com/ulicae/cinelog/io/exportdb/ExportFragment.java
+++ app/src/main/java/com/ulicae/cinelog/io/exportdb/ExportFragment.java
@@ -1,58 +1,39 @@
 package com.ulicae.cinelog.io.exportdb;
 
-import android.Manifest;
-import android.app.Application;
-import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
+import androidx.documentfile.provider.DocumentFile;
 import androidx.fragment.app.Fragment;
 
 import com.ulicae.cinelog.KinoApplication;
-import com.ulicae.cinelog.R;
-import com.ulicae.cinelog.android.v2.activities.MainActivity;
-import com.ulicae.cinelog.data.services.wishlist.MovieWishlistService;
-import com.ulicae.cinelog.data.services.wishlist.SerieWishlistService;
 import com.ulicae.cinelog.databinding.ActivityExportDbBinding;
+import com.ulicae.cinelog.io.exportdb.exporter.ExporterFactory;
 import com.ulicae.cinelog.io.exportdb.exporter.MovieCsvExporterFactory;
 import com.ulicae.cinelog.io.exportdb.exporter.SerieCsvExporterFactory;
 import com.ulicae.cinelog.io.exportdb.exporter.TagCsvExporterFactory;
 import com.ulicae.cinelog.io.exportdb.exporter.WishlistCsvExporterFactory;
+import com.ulicae.cinelog.room.entities.ItemEntityType;
+import com.ulicae.cinelog.room.services.WishlistAsyncService;
 
 public class ExportFragment extends Fragment {
 
     private @NonNull ActivityExportDbBinding binding;
-
-    private Boolean writeStoragePermission;
-
-    private final ActivityResultLauncher<String> requestPermissionLauncher =
-            registerForActivityResult(
-                    new ActivityResultContracts.RequestPermission(), isGranted -> {
-                        writeStoragePermission = isGranted;
-                    }
-            );
-
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
-
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            writeStoragePermission = true;
-        } else {
-            requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        }
 
         binding = ActivityExportDbBinding.inflate(getLayoutInflater());
         return binding.getRoot();
@@ -65,23 +46,58 @@ public class ExportFragment extends Fragment {
         binding.exportInDbContent.exportDbButton.setOnClickListener(this::onClick);
     }
 
-    public void onClick(View view) {
-        Application app = requireActivity().getApplication();
-        if (writeStoragePermission != null && writeStoragePermission) {
-            new SnapshotExporter(new TagCsvExporterFactory(
-                    ((KinoApplication) requireActivity().getApplication()).getDb()),
-                    app).export("export_tags.csv");
-            new SnapshotExporter(new MovieCsvExporterFactory(app), app).export("export_movies.csv");
-            new SnapshotExporter(new SerieCsvExporterFactory(app), app).export("export_series.csv");
-            new SnapshotExporter(new WishlistCsvExporterFactory(
-                    new MovieWishlistService(((KinoApplication) app).getDaoSession())),
-                    app).export("export_wishlist_movies.csv");
-            new SnapshotExporter(new WishlistCsvExporterFactory(
-                    new SerieWishlistService(((KinoApplication) app).getDaoSession())),
-                    app).export("export_wishlist_series.csv");
-        } else {
-            Toast.makeText(app.getApplicationContext(), getString(R.string.export_permission_error_toast), Toast.LENGTH_LONG).show();
+    private final ActivityResultCallback<Uri> activityResultCallback = result -> {
+        DocumentFile choosenDirFile = DocumentFile.fromTreeUri(requireActivity(), (Uri) result);
+        KinoApplication app = ((KinoApplication) requireActivity().getApplication());
+
+        exportData(app, choosenDirFile);
+    };
+
+    private void exportData(KinoApplication app, DocumentFile documentFile) {
+        exportForType(
+                app,
+                documentFile,
+                "export_wishlist_series.csv",
+                new WishlistCsvExporterFactory(new WishlistAsyncService(app.getDb(), ItemEntityType.SERIE))
+        );
+
+        exportForType(
+                app,
+                documentFile,
+                "export_wishlist_movies.csv",
+                new WishlistCsvExporterFactory(new WishlistAsyncService(app.getDb(), ItemEntityType.MOVIE))
+        );
+
+        exportForType(
+                app,
+                documentFile,
+                "export_tags.csv",
+                new TagCsvExporterFactory(((KinoApplication) requireActivity().getApplication()).getDb())
+        );
+
+        exportForType(app, documentFile, "export_movies.csv", new MovieCsvExporterFactory(app));
+        exportForType(app, documentFile, "export_series.csv", new SerieCsvExporterFactory(app));
+    }
+
+    private void exportForType(KinoApplication app, DocumentFile documentFile,
+                               String exportFilename, ExporterFactory exporterFactory) {
+        DocumentFile exportFile = documentFile.createFile("application/text", exportFilename);
+        if(exportFile == null){
+            return;
         }
+
+        new SnapshotExporter(
+                exporterFactory,
+                app,
+                requireActivity().getContentResolver()
+        ).export(exportFile.getUri());
+    }
+
+
+    ActivityResultLauncher<Uri> launcher = registerForActivityResult(new ActivityResultContracts.OpenDocumentTree(), activityResultCallback);
+
+    public void onClick(View view) {
+        launcher.launch(Uri.fromFile(requireActivity().getFilesDir()));
     }
 
 }
--- app/src/main/java/com/ulicae/cinelog/io/exportdb/SnapshotExporter.java
+++ app/src/main/java/com/ulicae/cinelog/io/exportdb/SnapshotExporter.java
@@ -1,47 +1,48 @@
 package com.ulicae.cinelog.io.exportdb;
 
 import android.app.Application;
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
 import android.widget.Toast;
 
 import com.ulicae.cinelog.R;
 import com.ulicae.cinelog.io.exportdb.exporter.CsvExporter;
 import com.ulicae.cinelog.io.exportdb.exporter.ExporterFactory;
 
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class SnapshotExporter {
+    private final ExporterFactory exporterFactory;
+    private final Application application;
 
-    private ExporterFactory exporterFactory;
-    private Application application;
+    private final ContentResolver contentResolver;
 
-    SnapshotExporter(ExporterFactory exporterFactory, Application application) {
+    SnapshotExporter(ExporterFactory exporterFactory, Application application,
+                          ContentResolver contentResolver) {
         this.exporterFactory = exporterFactory;
         this.application = application;
+        this.contentResolver = contentResolver;
     }
 
-    public void export(String exportFilename) {
+    public void export(Uri exportFilename) {
         showToast(R.string.export_start_toast);
 
-        CsvExporter csvExporter;
-        FileWriter fileWriter;
         try {
-            fileWriter = new FileWriterGetter(application)
-                    .get(exportFilename);
-        } catch (IOException e) {
-            showToast(R.string.export_io_error_toast);
-            return;
-        }
+            ParcelFileDescriptor parcelFileDescriptor = contentResolver.openFileDescriptor(exportFilename, "w");
+            if(parcelFileDescriptor == null) {
+                throw new IOException();
+            }
 
-        try {
-            csvExporter = exporterFactory.makeCsvExporter(fileWriter);
-        } catch (IOException e) {
-            showToast(R.string.export_io_error_toast);
-            return;
-        }
+            FileDescriptor fd = parcelFileDescriptor.getFileDescriptor();
+            FileWriter fileWriter = new FileWriter(fd);
 
-        try {
+            CsvExporter csvExporter = exporterFactory.makeCsvExporter(fileWriter);
             csvExporter.export();
+
             showToast(R.string.export_succeeded_toast);
         } catch (IOException e) {
             showToast(R.string.export_io_error_toast);
@@ -51,5 +52,6 @@ public class SnapshotExporter {
     private void showToast(int messageId) {
         Toast.makeText(application.getApplicationContext(), application.getString(messageId), Toast.LENGTH_LONG).show();
     }
+
 
 }
