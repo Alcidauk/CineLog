8c33624cccc4b35206abb35a6463f036e4d58616
Pierre R <contact@ulicae.com>
2025-02-05 10:44:24 +0100

    [Cinelog / Room] remove old service class, now useless since we use async services

--- app/src/main/java/com/ulicae/cinelog/network/KinoBuilderFromMovie.java
+++ app/src/main/java/com/ulicae/cinelog/network/KinoBuilderFromMovie.java
@@ -13,7 +13,7 @@ import java.util.Locale;
  *
  * This file is part of CineLog.
  * CineLog is free software: you can redistribute it and/or modify
- * it under tSerieReviewServicehe terms of the GNU General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
